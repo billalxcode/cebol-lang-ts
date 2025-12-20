dc91ca1d680768e92b95fe3774b53a8efaaf68e8
Billal Fauzan <billal.xcode@gmail.com>
2025-12-20 10:47:58 +0700

    feat(build): add build script for Cebol to automate project compilation

---
+++ scripts/build.ts
@@ -0,0 +1,15 @@
+async function main() {
+	console.log("Building Cebol...")
+
+	await Bun.build({
+		entrypoints: ["./index.ts"],
+		outdir: `./bin/cebol`,
+		compile: true,
+		target: "node",
+	})
+
+	console.log("Build complete!")
+}
+
+
+await main()
